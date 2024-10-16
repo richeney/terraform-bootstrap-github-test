3fcb00522a4a273ed22a012a13241837716beb45
Richard Cheney <24895350+richeney@users.noreply.github.com>
2024-10-16 11:03:17 +0100

    Add main.tf

--- main.tf
+++
@@ -1,9 +0,0 @@
-provider "azurerm" {
-    features {}
-    subscription_id = "3382c75c-900e-4eec-a55e-abeae7647561"
-}
-
-resource "azurerm_resource_group" "deleteme" {
-    name     = "terraform-bootstrap-github-deleteme"
-    location = "UK South"
-}
